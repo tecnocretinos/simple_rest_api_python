f5bef56e11e30453710340c06886d5a3f547b737
yorjaggy <jhaggy14@gmail.com>
2021-07-15 23:35:28 -0500

    adding CRUD to user API

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask
+import random
+from flask import Flask, json, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hola Tecno-Cretino!</p>"
+dummy_json = {
+    "nohlkjnknlkj": {   
+        "id": "nohlkjnknlkj",
+        "name": "Elon Musk",
+        "phone_number" : 3133007011,
+        "address": "Mars",
+        "iluminati": True
+    },
+    "asdasdha": {
+        "id": "asdasdha",
+        "name": "Steven Trabajo",
+        "phone_number" : 3114005392,
+        "address": "iCloud",
+        "iluminati": False
+    }
+}
+
+@app.route('/users/<id>', methods=['GET'])
+def UserById(id):
+    if request.method == 'GET':
+        return jsonify(dummy_json[id])
+
+
+@app.route('/users/', methods=['GET','POST', 'PUT', 'DELETE'])
+def Users():
+    if request.method == 'GET':
+        return jsonify(dummy_json)
+
+    elif request.method == 'POST':
+        params = request.json
+        id_hash = get_short_hash()
+        params["id"] = id_hash
+        dummy_json[id_hash] = params
+        return jsonify(params)
+
+    elif request.method == 'PUT':
+        params = request.json
+
+        if(params['id'] in dummy_json.keys()):
+            dummy_json[params['id']] = params
+            return jsonify(dummy_json)
+        else:
+            id_hash = get_short_hash()
+            params.id = id_hash
+            dummy_json[id_hash] = params
+            return jsonify(params)
+
+    elif request.method == 'DELETE':
+        delete_id = request.args.get('id')
+        dummy_json.pop(delete_id)
+        return jsonify(dummy_json)
+
+def get_short_hash():
+    hash = hex(random.getrandbits(128))[2:-1]
+    return hash[:10]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
